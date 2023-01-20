8e756df5094657163d5f5e834c096fa5761c8103
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-01-20 23:32:13 +0100

    Admin class created, SchoolName Test and Methods Created

---
+++ src/test/java/AdminTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.ironschool.Admin;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
+public class AdminTest {
+    @Test
+    public void SchoolRegisterAName_WhenTheCreateSchoolMethodIsCalled() {
+        // arrange
+        String expectedName = "IronHack";
+
+        // act
+        Admin.createSchoolName(expectedName);
+        String actualName = Admin.getSchoolName();
+
+        // assert
+        assertEquals("Ironhack", actualName);
+    }
+
+    @Test
+    public void SchoolNameEmpty_WhenCreateSchoolMethodIsCalled(){
+        // arrange
+        String expectedName = "";
+        try {
+            // act
+            Admin.createSchoolName(expectedName);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // assert
+            assertEquals("The School Name cannot remain empty", e.getMessage());
+        }
+    }
+}
+
+
