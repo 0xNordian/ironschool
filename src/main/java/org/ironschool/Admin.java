efc7d17945757a32e7a147c0bdd753a3fabeaa4e
jordi92al <jordi92al@gmail.com>
2023-01-25 20:58:52 +0100

    comenzando hashmap

--- src/main/java/org/ironschool/Admin.java
+++ src/main/java/org/ironschool/Admin.java
@@ -16,16 +16,21 @@ public abstract class Admin {
     }
     //public static void assign(Teacher teacher, Course course){}
 
-
+  /*
+    public static void show ( School school, LookupType type, String id){
+        switch (type) {
+            case STUDENT -> System.out.println(school.getStudents());
+            case COURSE -> System.out.println(school.getCourses());
+            case TEACHER -> System.out.println(school.getTeachers());
+        }
+    }
+*/
     public static void show(School school, String findBy){
      if(findBy.equals("student")||findBy.equals("course")||findBy.equals("teacher")) {
          switch (findBy) {
-             case "student":
-                 System.out.println(school.getStudents());
-             case "course":
-                 System.out.println(school.getCourses());
-             case "teacher":
-                 System.out.println(school.getTeachers());
+             case "student" -> System.out.println(school.getStudents());
+             case "course" -> System.out.println(school.getCourses());
+             case "teacher" -> System.out.println(school.getTeachers());
          }
       }else{
          throw new IllegalArgumentException("Can't find your query");
